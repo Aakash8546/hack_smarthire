c7988885e446e8ff6c83f8e4ac0ef2a0313149fd
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2026-04-01 00:32:54 +0530

    updating appln service

--- src/main/java/com/smarthire/service/ResumeService.java
+++ src/main/java/com/smarthire/service/ResumeService.java
@@ -1,6 +1,9 @@
 package com.smarthire.service;
 
+import com.smarthire.dto.candidate.ResumeAnalysisResponse;
+import com.smarthire.dto.candidate.ResumeExistsResponse;
 import com.smarthire.dto.candidate.ResumeUploadResponse;
+import com.smarthire.dto.common.ResumeDownloadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ResumeService {
@@ -8,4 +11,10 @@ public interface ResumeService {
     ResumeUploadResponse uploadResume(MultipartFile file);
 
     String deleteResume();
+
+    ResumeDownloadResponse downloadResume(Long candidateId);
+
+    ResumeExistsResponse hasResume(Long candidateId);
+
+    ResumeAnalysisResponse getResumeAnalysis(Long candidateId);
 }
