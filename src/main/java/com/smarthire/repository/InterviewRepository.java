5337f90b80af91e959b7d1c255e2c135d8ddf257
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2026-04-01 22:26:35 +0530

    added find cand by id and type

--- src/main/java/com/smarthire/repository/InterviewRepository.java
+++ src/main/java/com/smarthire/repository/InterviewRepository.java
@@ -1,6 +1,7 @@
 package com.smarthire.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.smarthire.entity.Interview;
 import com.smarthire.entity.User;
@@ -10,4 +11,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
 
     List<Interview> findAllByCandidateAndTypeOrderByCreatedAtDesc(User candidate, InterviewType type);
+
+    Optional<Interview> findByIdAndCandidateAndType(Long id, User candidate, InterviewType type);
 }
