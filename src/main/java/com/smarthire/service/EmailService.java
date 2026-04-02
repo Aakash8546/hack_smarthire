d7020d4513de8a12d80f55bff6504603a554cb58
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2026-04-02 16:56:35 +0530

    creating fuctions for email service

--- src/main/java/com/smarthire/service/EmailService.java
+++ src/main/java/com/smarthire/service/EmailService.java
@@ -1,8 +1,13 @@
 package com.smarthire.service;
 
-import com.smarthire.entity.User;
+import com.smarthire.entity.enums.ApplicationStatus;
 
 public interface EmailService {
 
-    void sendOtpEmail(User user, String otp);
+    void sendOtpEmail(com.smarthire.entity.User user, String otp);
+
+    void sendJobPostedEmail(String candidateEmail, String candidateName, String jobTitle, String companyName, String jobPackage, String location);
+
+    void sendApplicationStatusUpdatedEmail(String candidateEmail, String candidateName, String jobTitle, String companyName,
+                                           ApplicationStatus status, String optionalMessage);
 }
