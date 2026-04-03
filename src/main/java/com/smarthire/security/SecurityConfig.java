b230a57ee740a872aa52fafa7e361778d0ee8dd6
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2026-04-03 18:09:58 +0530

    updating cors

--- src/main/java/com/smarthire/security/SecurityConfig.java
+++ src/main/java/com/smarthire/security/SecurityConfig.java
@@ -112,6 +112,8 @@ public class SecurityConfig {
         origins.add("http://127.0.0.1:*");
         origins.add("https://localhost:*");
         origins.add("https://127.0.0.1:*");
+        origins.add("https://smarthire001.netlify.app");
+
 
         return origins.stream().distinct().toList();
     }
